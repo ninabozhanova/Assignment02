1a9ff9fef9e8b905e8cd5ccad7152aad8e07ef81
Nina Bozhanova <nbozhanova@gmail.com>
2024-02-22 20:20:16 -0800

    perfecting....

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -10,21 +10,24 @@ choice = prompt('Select Heads or Tails (h/t)')
 if (randomNum > 0.5) {
     coinFlip = 't'
 } else {
-    coinFlip = "h"
+    coinFlip = 'h'
 }
 
-// SELECT THE RESULT
-
-if (coinFlip === "h") {
-    if (choice === "h") {
-        alert('The flip was heads and you chose heads...you win!')
-    } else {
-        alert('The flip was heads but you chose tails...you lose!')
-    }
+// SELECT THE RESULT IF THE PLAYER'S INPUT WAS VALID
+if (!(choice === 'h' || choice === 't')) {
+    alert(`\'${choice}\' is not a valid selection!`)
 } else {
-    if (choice === "h") {
-        alert('The flip was tails but you chose heads...you lose!')
+    if (coinFlip === 'h') {
+        if (choice === 'h') {
+            alert('The flip was heads and you chose heads...you win!')
+        } else {
+            alert('The flip was heads but you chose tails...you lose!')
+        }
     } else {
-        alert('The flip was tails and you chose tails...you win!')
+        if (choice === 'h') {
+            alert('The flip was tails but you chose heads...you lose!')
+        } else {
+            alert('The flip was tails and you chose tails...you win!')
+        }
     }
 }
